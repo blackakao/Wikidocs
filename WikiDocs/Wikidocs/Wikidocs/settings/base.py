85c10ed09f2c2577617984bc6db878672c1a3936
박재범 <pass95123@naver.com>
2022-02-15 16:31:42 +0900

    settings.py modify

--- WikiDocs/Wikidocs/Wikidocs/settings/base.py
+++ WikiDocs/Wikidocs/Wikidocs/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fx^ty=06=hu)^6bs1r4rl0uryilj$ktaci_-w%4f^(^6bvpbid
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.39.15.253']
+ALLOWED_HOSTS = []
 
 
 # Application definition
---
+++ WikiDocs/Wikidocs/Wikidocs/settings/prod.py
@@ -0,0 +1,3 @@
+from .base import *
+
+ALLOWED_HOSTS = ['15.165.202.215'] #일단 위키독스처럼 따라함
